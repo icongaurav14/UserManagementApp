c5522ea2063f0c6f96acb4407450bf967c694d57
Gaurav Srivastava <icongaurav14@gmail.com>
2017-12-01 23:03:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const redis = require('redis');
+
+
+//create redis client
+let client = redis.createClient();
+client.on('connect', function(){
+    console.log("redis connected");
+});
+
+//setup port
+
+const port = 3000;
+
+//Init app var
+const app = express();
+
+//view engine
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+//bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//methodoverride
+app.use(methodOverride('_method'));
+
+//search page
+ 
+app.get('/',function(req,res,next){
+    res.render('searchusers');
+});
+
+//search processing
+
+app.post('/user/search', function(req,res,next){
+    let id = req.body.id;
+    client.hgetall(id, function(err, obj){
+        if(!obj){
+            res.render('searchusers', {
+                error: "User does not exist"
+            })
+        }
+        else{
+            obj.id = id;
+            res.render('details',{
+                user:obj
+            });
+        }
+    });
+});
+
+
+//add user page
+app.get('/user/adduser', function(req,res,next){
+    res.render("adduser");
+});
+
+
+//save user page
+app.post('/user/add', function(req,res,next){
+    let id = req.body.id;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let email = req.body.email;
+    let phone = req.body.phone;
+
+    client.hmset(id, [
+        'first_name', first_name,
+        'last_name', last_name,
+        'email', email,
+        'phone', phone
+    ], function(err, reply){
+        if(err){
+            console.log(err);
+        }
+        console.log(reply);
+        res.redirect('/');
+    });
+});
+
+
+app.listen(port, function(){
+    console.log("server started on port " + port);
+});
